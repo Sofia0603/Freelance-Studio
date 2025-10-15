2c056b0d9421e00d7f062e661330e87b7493144b
Sofia Shestakova <shestakova.sofia2016@mail.ru>
2025-10-15 13:20:40 +0400

    fix

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,5 +1,60 @@
 export class Login{
+
   constructor(){
-    console.log('login')
+    this.emailElement = document.getElementById('email');
+    this.passwordElement = document.getElementById('password')
+    this.rememberMeElement = document.getElementById('remember-me')
+    document.getElementById('process-button').addEventListener('click', this.login.bind(this));
+  }
+
+
+  validateForm(){
+    let isValid = true;
+
+    if (this.emailElement.value && this.emailElement.value.match(/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/)){
+      this.emailElement.classList.remove('is-invalid');
+    } else {
+      this.emailElement.classList.add('is-invalid');
+      isValid = false;
+    }
+
+    if (this.passwordElement.value ){
+      this.passwordElement.classList.remove('is-invalid');
+    } else {
+      this.passwordElement.classList.add('is-invalid');
+      isValid = false
+    }
+
+    return isValid;
+  }
+
+
+ async login(){
+    if(this.validateForm()){
+
+     const response = await fetch('http://localhost:3000/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+          email:this.emailElement.value,
+          password:this.passwordElement.value,
+          rememberMe: this.rememberMeElement.checked
+        })
+      });
+      const result = await response.json()
+
+      if(result.error || !result.accessToken || !result.refreshToken || !result.id || !result.name){
+
+      }
+
+      console.log(result)
+
+    } else {
+
+
+    }
   }
 }
